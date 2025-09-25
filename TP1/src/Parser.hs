da491870f351a36ae857518e462dbc4f0ec8cf54
Ignacio Basualdo <82559716+IgnacioBasu@users.noreply.github.com>
2025-09-25 20:28:40 -0300

    Update Parser.hs

--- TP1/src/Parser.hs
+++ TP1/src/Parser.hs
@@ -41,11 +41,61 @@ lis = makeTokenParser
     }
   )
 
+identifierT   = identifier lis      -- variables/identificadores
+reservedT     = reserved lis        -- palabras clave
+reservedOpT   = reservedOp lis      -- operadores
+parensT       = parens lis          -- ( expr )
+bracesT       = braces lis          -- { stmts }
+semiT         = semi lis            -- ;
+commaT        = comma lis           -- ,
+naturalT      = natural lis         -- números naturales
+integerT      = integer lis         -- enteros con signo
+whiteSpaceT   = whiteSpace lis      -- ignora espacios/comentarios
+symbolT       = symbol lis          -- símbolos como "="
+
+
+
 -----------------------------------
 --- Parser de expresiones enteras
 -----------------------------------
+parseConst :: Parser (Exp Int)
+parseConst = do
+    n <- integerT
+    return (Const (fromInteger n))
+
+parseVar :: Parser (Exp Int)
+parseVar = do
+    v <- identifierT
+    return (Var v)
+
+parseFactor :: Parser (Exp Int)
+parseFactor = do
+      parseConst
+  <|> do parseVar
+  <|> do expr <- parensT intexp
+         return expr
+
+mulOp :: Parser (Exp Int -> Exp Int -> Exp Int)
+mulOp = do
+    reservedOpT "*"
+    return Times
+  <|> do
+    reservedOpT "/"
+    return Div
+
+addOp :: Parser (Exp Int -> Exp Int -> Exp Int)
+addOp = do
+    reservedOpT "+"
+    return Plus
+  <|> do
+    reservedOpT "-"
+    return Minus
+
+parseTerm :: Parser (Exp Int)
+parseTerm = parseFactor `chainl1` mulOp
+
 intexp :: Parser (Exp Int)
-intexp = undefined
+intexp = parseTerm `chainl1` addOp
 
 ------------------------------------
 --- Parser de expresiones booleanas
