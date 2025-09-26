f53b08644ab81f0ed0c9b936242edc83fd3294bc
Lautycape <lautarocapezio1@gmail.com>
2025-09-25 21:55:39 -0300

    Update Parser.hs

--- TP1/src/Parser.hs
+++ TP1/src/Parser.hs
@@ -37,6 +37,7 @@ lis = makeTokenParser
                         , "!="
                         , ";"
                         , ","
+                        , "++"
                         ]
     }
   )
